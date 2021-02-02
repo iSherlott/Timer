524c5b08d326f8df2f3e8ac336453eabb09e41f3
iSherlott <sherlott@live.com>
2021-02-02 00:03:49 -0300

    end project

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-"# Timer" 
+# Timer
+
+Temporizador feito com HTML5 + CSS + Javascript; baseado na atividade do curso de Javascript da Udemy do professor `Luiz Otávio` referente a Aula 64 da Sessão 3 <br />
+<a href="https://www.udemy.com/course/curso-de-javascript-moderno-do-basico-ao-avancado">Link do curso</a> <br />
+
+## Installation
+
+Abra o cmd na pasta que deseja descarregar o repositorio, após execute o comando abaixo: <br />
+`git clone https://github.com/iSherlott/Timer.git`
---
+++ assets/js/main.js
@@ -0,0 +1,59 @@
+let seconds = 0;
+let minutes = 0;
+let hours = 0;
+
+let timer;
+
+let statusCount = true;
+
+function formatTime(num) {
+  return num >= 10 ? num : `0${num}`;
+}
+
+function counter() {
+  seconds += 1;
+
+  if (seconds > 10) {
+    seconds = 0;
+    minutes += 1;
+  }
+
+  if (minutes > 10) {
+    minutes = 0;
+    hours += 1;
+  }
+
+  hours > 24 ? (hours = 0) : hours;
+
+  return `${formatTime(hours)}:${formatTime(minutes)}:${formatTime(seconds)}`;
+}
+
+function start() {
+  if (statusCount) {
+    timer = setInterval(function () {
+      document.querySelector(".content").innerText = counter();
+      document.querySelector(".content").style.color = "green";
+    }, 1000);
+    statusCount = false;
+  }
+}
+
+function pause() {
+  if (!statusCount) {
+    clearInterval(timer);
+    document.querySelector(".content").style.color = "red";
+    statusCount = true;
+  }
+}
+
+function restart() {
+  seconds = 0;
+  minutes = 0;
+  hours = 0;
+
+  document.querySelector(".content").innerText = `${formatTime(
+    hours
+  )}:${formatTime(minutes)}:${formatTime(seconds)}`;
+
+  document.querySelector(".content").style.color = "black";
+}
